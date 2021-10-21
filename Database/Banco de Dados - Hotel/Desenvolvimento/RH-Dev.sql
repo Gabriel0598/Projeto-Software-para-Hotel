f828658bef168aeb89c64c701d5ce1d31cd20b56
Gabriel <augustogabriel35@gmail.com>
2021-10-21 12:10:03 -0300

    GF16 - Tabela do Recursos Humanos

---
+++ Database/Banco de Dados - Hotel/Desenvolvimento/RH-Dev.sql
@@ -0,0 +1,15 @@
+USE UniHotel_Desenvolvimento
+GO
+
+CREATE TABLE RH
+(
+Centro_Custo TINYINT PRIMARY KEY NOT NULL,
+--Comunicação:
+Ramal_Principal SMALLINT,
+Email_Principal VARCHAR(50),
+Cargos VARCHAR(50),
+Ficha_Func VARCHAR(50),
+Contrat_Trab INT,
+)
+
+SELECT * FROM RH;
