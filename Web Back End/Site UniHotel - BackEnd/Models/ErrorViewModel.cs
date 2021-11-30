e50a91657382d9adf81a75a0eade7fd9a839e14c
Gabriel <augustogabriel35@gmail.com>
2021-11-30 12:34:52 -0300

    GF2 - Model

---
+++ Web Back End/Site UniHotel - BackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Site_UniHotel___BackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
